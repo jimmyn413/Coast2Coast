feb4463587f990ed5c74becbf4af39159e6ab61c
Jimmy <Jimmy@DESKTOP-28MVTHI>
2016-09-18 11:25:06 -0700

    Add project files.

---
+++ Coast2Coast/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Coast2Coast.MvcApplication" Language="C#" %>
---
+++ Coast2Coast/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Coast2Coast.Startup))]
+namespace Coast2Coast
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
